7efe4b7b8ea35b97961fc2f92af4630a7015b696
fsilva1984 <fsilva916@hotmail.com>
2023-04-16 19:03:35 -0300

    Windows 11

---
+++ First_Steps/Comments.py
@@ -0,0 +1,14 @@
+
+
+print(" charp '#' para comentario inline ou aspas triplas no inicio e no fim para comments block")
+
+
+print("Primeira linha")
+# print("Segunda linha")
+print("Terceira linha")
+
+""" 
+Nada aqui sera 
+mostrado !
+
+"""
