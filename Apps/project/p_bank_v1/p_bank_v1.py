447e0d599e87e7f76ac400b210d8bf93f4767827
fsilva1984 <fsilva916@hotmail.com>
2024-05-06 09:17:40 -0400

    Mint lmde

---
+++ Apps/project/p_bank_v1/p_bank_v1.py
@@ -0,0 +1,36 @@
+
+print(f'''
+{' Welcome the internet bank '.center(36, '*')}
+
+{' Menu '.center(24, '-')}
+  
+  [0] - for exit
+  [1] - for deposit
+  [2] - for extract
+  [3] - for balance
+
+{''.center(25, '-')}
+''')
+
+
+choices = -1
+
+while choices != 0:
+  
+  if condition:
+    pass
+  depositos = float(input('Valor R$: '))
+  saques = float(input)
+  
+
+
+
+
+# extrato = ''
+
+# if depositos:
+#   extrato += depositos
+# else:
+#   extrato += saques
+  
+# print(extrato)
---
+++ metodos_de_class/metodos_da_class_string/fatiar_strings.py
@@ -0,0 +1,47 @@
+
+
+name = 'Flavio F. Silva'
+
+# pegando uma caractere pelo seu indice na cadeia.
+
+print(f'{name[0]} {name[1]} {name[2]}')
+
+print('')
+# pegando caracteres do final da string
+
+print(name[-5])
+
+print('')
+# pegando uma cadeia de caracteres do indice 0 ate o 9
+
+print(name[:9])
+
+print('')
+# pegando uma cadeia de caracteres do indice 9 ate o final da string
+
+print(name[9:])
+
+
+print('')
+# pegando uma cadeia de caracteres passando o indice inicial e o final
+
+print(name[7:15])
+
+
+print('')
+# pegando uma cadeia de caracteres passando o indice inicial o final e
+# de quantos em quantos passos se dara.
+
+print(name[0:15:5])
+
+
+print('')
+# pegando toda a cadeia de caracteres
+ 
+print(name[:])
+
+
+print('')
+# espelhando toda a cadeia de caracteres
+
+print(name[:: -1])
